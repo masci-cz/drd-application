d2beb6fea5e7a9b847babdb8601ad29ac7c6b447
Daniel Mašek <daniel@masci.cz>
2021-06-14 23:05:44 +0200

    Add embedded Mongo DB dependency

---
+++ app/src/main/java/cz/masci/dd/app/runner/Runner.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2021 Daniel
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package cz.masci.dd.app.runner;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author Daniel
+ */
+@Slf4j
+@Component
+public class Runner implements ApplicationRunner {
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        log.info("Running DD application");
+    }
+
+}
