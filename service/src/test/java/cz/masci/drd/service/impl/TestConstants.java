2595fb61c990341ebbe5ce463c7cc34399f10306
Daniel Mašek <daniel@masci.cz>
2021-09-18 18:33:58 +0200

    DD-4: Add service tests

---
+++ service/src/test/java/cz/masci/drd/service/impl/TestConstants.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2021 Daniel
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package cz.masci.drd.service.impl;
+
+/**
+ *
+ * @author Daniel
+ */
+public class TestConstants {
+
+    public static final String EXPERIENCE = "Experience";
+    public static final String TREASURE = "Treasure";
+    public static final int INTELLIGENCE = 11;
+    public static final String MOVEABILITY = "Moveability";
+    public static final String VULNERABILITY = "Vulnerability";
+    public static final String DIMENSION = "A";
+    public static final int ENDURANCE = 10;
+    public static final String DEFENCE = "Defence";
+    public static final String ATTACK = "Attack";
+    public static final String VIABILITY = "Viability";
+    public static final String MONSTER_NAME = "Monster name";
+}
