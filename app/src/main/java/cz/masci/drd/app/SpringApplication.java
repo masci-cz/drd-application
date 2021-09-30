3a01bdd1b1a849da81b7dba737ea57e2ab3b9d23
Daniel Mašek <daniel@masci.cz>
2021-09-30 08:35:07 +0200

    DD-10: Add testing scene

--- app/src/main/java/cz/masci/drd/app/SpringApplication.java
+++ app/src/main/java/cz/masci/drd/app/SpringApplication.java
@@ -1,11 +1,22 @@
-package cz.masci.drd.app;
+/*
+ * Copyright (C) 2021 Daniel
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */package cz.masci.drd.app;
 
 import javafx.application.Application;
-import net.rgielen.fxweaver.core.FxWeaver;
-import net.rgielen.fxweaver.spring.SpringFxWeaver;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(scanBasePackages = "cz.masci.drd")
 public class SpringApplication {
@@ -14,11 +25,4 @@ public class SpringApplication {
         System.out.println("SpringApplication - main");
         Application.launch(JavaFxApplication.class, args);
     }
-
-//    @Bean
-//    public FxWeaver fxWeaver(ConfigurableApplicationContext applicationContext) {
-//        // Would also work with javafx-weaver-core only:
-//        // return new FxWeaver(applicationContext::getBean, applicationContext::close);
-//        return new SpringFxWeaver(applicationContext); 
-//    }
 }
